24a7591cc1fe1665a25577db8e55f6ea4c8f4dae
Vito Gentile <gentilevito1076@gmail.com>
2021-02-25 20:38:44 +0100

    DUMP su paragrafo non sottoposto a EXPLODE FX

--- BONUS/index.php
+++ BONUS/index.php
@@ -89,6 +89,18 @@
 
      ?>
 <!-- ----------------------------------------------------------------------------------------------->
+  <br><br>
+
+  <h3>var_dump($nuovoParagrafo) stamperà qui sotto l'arrey completo:</h3>
+  <br>
+  <?php
+  $nuovoParagrafo = 'LOREM ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+  var_dump($nuovoParagrafo);
+
+   ?>
+
+
+<!-- ------------------------------------------------------------------------------------------------------ -->
     <br><br>
 
     <h3>TRIM FX - trim(stringa) - toglierà gli spazi bianchi all’inizio e alla fine di una stringa</h3>
