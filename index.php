6c375e1a5d9502338dc5cabb6c8d6302122c4df2
Vito Gentile <gentilevito1076@gmail.com>
2021-02-25 16:27:05 +0100

    Commenti

--- index.php
+++ index.php
@@ -8,10 +8,13 @@
 
     <?php
 
+      //Creo una variabile contenente un paragrafo di testo:
       $paragrafo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
+      //Stampo a schermo, il contenuto della var paragrafo:
       // echo $paragrafo;
 
+      //Sostituisco la "bad word" passata in GET (http://localhost/php-badwords/index.php?nome=Lorem) con 3 asterischi:
       $paragrafo = str_replace($_GET['nome'], '***', $paragrafo);
       echo $paragrafo;
 
