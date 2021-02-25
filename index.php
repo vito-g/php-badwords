283f841fc094041d9f084fd7adde064f99233256
Vito Gentile <gentilevito1076@gmail.com>
2021-02-25 17:36:05 +0100

    Solo un po' di ordine

--- index.php
+++ index.php
@@ -6,22 +6,49 @@
   </head>
   <body>
 
+    <h2>Titolo del paragrafo</h2>
+
+    <!-- START First Section PHP Code-->
     <?php
 
-      //Creo una variabile contenente un paragrafo di testo:
-      $paragrafo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    //Creo una variabile contenente un paragrafo di testo:
+    $paragrafo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    //Stampo a schermo, il contenuto della var 'paragrafo':
+    echo $paragrafo;
 
-      //Stampo a schermo, il contenuto della var 'paragrafo':
-      // echo $paragrafo;
+     ?>
+     <!-- END First Section PHP Code-->
 
-      //Calcola la lunghezza di una stringa e la stampa a schermo attraverso echo:
-      echo strlen ( $paragrafo ); 
 
-      //Sostituisco la "bad word" ("Lorem" in questo caso) passata in GET (http://localhost/php-badwords/index.php?nome=Lorem) con 3 asterischi:
-      $paragrafo = str_replace($_GET['nome'], '***', $paragrafo);
-      echo $paragrafo;
+    <h4>La lunghezza del paragrafo è di caratteri:
 
-    ?>
+
+      <!-- START Second Section PHP Code-->
+      <?php
+
+        //Calcola la lunghezza di una stringa e la stampa a schermo attraverso 'echo':
+        echo strlen ( $paragrafo );
+
+      ?>
+      <!-- END Second Section PHP Code-->
+
+
+    </h4>
+
+
+    <h3>
+      Sostituisco la "bad word" passata in GET (http://localhost/php-badwords/index.php?badWord=Lorem) con 3 asterischi:
+    </h3>
+
+    <!-- START Third Section PHP Code-->
+    <?php
+
+    //Sostituisco la "bad word" ("Lorem" in questo caso) passata in GET (http://localhost/php-badwords/index.php?nome=Lorem) con 3 asterischi:
+    $paragrafo = str_replace($_GET['badWord'], '***', $paragrafo);
+    echo $paragrafo;
+
+     ?>
+     <!-- END Third Section PHP Code-->
 
   </body>
 </html>
