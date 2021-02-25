7388d931d9ba0bebab4c7b79966f8f890d311406
Vito Gentile <gentilevito1076@gmail.com>
2021-02-25 20:30:48 +0100

    Aggiornamento

--- BONUS/index.php
+++ BONUS/index.php
@@ -14,7 +14,7 @@
     //Creo una variabile contenente un paragrafo di testo:
     $paragrafo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
     //Stampo a schermo, il contenuto della var 'paragrafo':
-    echo 'echo $variabile - stampare il contenuto della variabile';
+
     echo $paragrafo;
 
     //END First Section PHP Code
@@ -44,8 +44,9 @@
     //START Third Section PHP Code
 
     //Sostituisco la "bad word" ("Lorem" in questo caso) passata in GET (http://localhost/php-badwords/index.php?nome=Lorem) con 3 asterischi:
-    echo 'str_replace(porzioneDaModificare, conCosa, stringa)';
+    // echo 'str_replace(porzioneDaModificare, conCosa, stringa)';
     $paragrafo = str_replace($_GET['badWord'], '***', $paragrafo);
+    $paragrafo = str_replace($_GET['newBadWord'], 'xxx', $paragrafo);
     echo $paragrafo;
 
     //END Third Section PHP Code
