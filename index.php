e1ec6efd262111559505fc9b132e7bb7639f7b47
Vito Gentile <gentilevito1076@gmail.com>
2021-02-25 17:14:33 +0100

    ESERCIZIO OK

--- index.php
+++ index.php
@@ -11,11 +11,11 @@
       //Creo una variabile contenente un paragrafo di testo:
       $paragrafo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-      //Stampo a schermo, il contenuto della var paragrafo:
+      //Stampo a schermo, il contenuto della var 'paragrafo':
       // echo $paragrafo;
 
       //Calcola la lunghezza di una stringa e la stampa a schermo attraverso echo:
-      echo strlen ( $paragrafo );
+      echo strlen ( $paragrafo ); 
 
       //Sostituisco la "bad word" ("Lorem" in questo caso) passata in GET (http://localhost/php-badwords/index.php?nome=Lorem) con 3 asterischi:
       $paragrafo = str_replace($_GET['nome'], '***', $paragrafo);
